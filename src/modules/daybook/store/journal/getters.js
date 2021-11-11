58fa18b047518cc89be399e369f3f74d655648ef
Leopoldo Miranda <leopoldo.miranda18@tectijuana.edu.mx>
2021-11-10 20:52:44 -0800

    Primer commit

---
+++ src/modules/daybook/store/journal/getters.js
@@ -0,0 +1,12 @@
+// export const myGetters  = (state) => {
+//   return state.algo
+// }
+
+export const getEntriesByTerm = (/*state*/) => {
+
+}
+
+export const getEntriesById = (/*state*/) => {
+
+}
+
---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,24 @@
+
+export default () => ({
+  isLoading: true,
+  entries: [
+    {
+      id: new Date().getTime(),
+      date: new Date().toDateString(),
+      text: 'Ipsum minim minim consequat fugiat. Ut occaecat ea nostrud nulla. Nisi excepteur consequat cupidatat nostrud. Proident ut id consequat ex esse occaecat aute laborum ipsum. Aute laboris veniam cillum proident nostrud anim qui in cillum aliqua in adipisicing tempor. Cupidatat mollit commodo dolor reprehenderit adipisicing. Excepteur amet fugiat sunt adipisicing tempor aliqua ut do eiusmod ut duis veniam non.',
+      picture: null,
+    },
+    {
+      id: new Date().getTime() + 1000,
+      date: new Date().toDateString(),
+      text: 'Mollit anim minim aliqua velit minim consectetur et. Et aute elit labore voluptate deserunt dolore labore deserunt eiusmod voluptate nostrud id. Duis cillum dolor sit ea. Commodo ullamco officia mollit aliquip ex ad irure tempor proident consectetur anim eu.',
+      picture: null,
+    },
+    {
+      id: new Date().getTime() + 2000,
+      date: new Date().toDateString(),
+      text: 'Eu ullamco amet cillum laboris occaecat eu nostrud. Nostrud cillum ut sit velit ut dolor culpa pariatur. Laborum eiusmod fugiat laborum esse ea nisi consequat irure tempor Lorem fugiat cupidatat sit voluptate. Occaecat mollit enim commodo ipsum anim proident ea. Do esse anim enim ut reprehenderit est eu occaecat consectetur non qui occaecat sit velit. Ex reprehenderit exercitation proident duis. Mollit ad velit Lorem sint amet velit ea veniam.',
+      picture: null,
+    }
+  ]
+})
